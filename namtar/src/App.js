abec98064d651017d4904392e5f53b51f3e9567c
SJuras <jurasstpko@gmailcom>
2021-11-30 20:39:22 +0100

    v1.1.1 initial commit, basic React.Js framework added.

---
+++ namtar/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Radi</h1>
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* Infoboxes */}
+      {/* Infoboxes */}
+      {/* Infoboxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>
+  );
+}
+
+export default App;
