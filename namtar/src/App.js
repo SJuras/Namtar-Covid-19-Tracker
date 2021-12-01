62be0311aa8ad91a7dc496cbbfcef5b0ed13deab
SJuras <jurasstpko@gmailcom>
2021-12-01 23:02:47 +0100

    v1.1.2 trying out new color scheme.

--- namtar/src/App.js
+++ namtar/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Radi</h1>
+      <div className="app__header">
+        <h1>Namtar Covid-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">option2</MenuItem>
+            <MenuItem value="worldwide">option3</MenuItem>
+            <MenuItem value="worldwide">option4</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
       {/* Header */}
+
       {/* Title + Select input dropdown field */}
 
       {/* Infoboxes */}
