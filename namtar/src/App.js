59303e5fbe0ea96649b2dfa0c97c86a822f9e440
SJuras <jurasstpko@gmailcom>
2021-12-09 14:59:19 +0100

    v1.1.3 basic markup for footer added.

--- namtar/src/App.js
+++ namtar/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 import './App.css';
 
+import NamtarLogo from './assets/namtarLogo.png';
+
 function App() {
   return (
     <div className="app">
       <div className="app__header">
-        <h1>Namtar Covid-19 Tracker</h1>
+        <div className="logo">
+          <img src={NamtarLogo} alt="namtar covid 19 tracker logo" />
+        </div>
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
             <MenuItem value="worldwide">Worldwide</MenuItem>
@@ -28,6 +32,49 @@ function App() {
       {/* Graph */}
 
       {/* Map */}
+
+      {/* Footer */}
+      <footer>
+        <div className="footer-row">
+          <div className="footer-box-smaller">
+            <h4>NAMTAR</h4>
+            <h5>COVID-19 Tracker</h5>
+          </div>
+          <div className="footer-box-bigger">
+            <div className="bigger-box">
+              <h4>Useful Links</h4>
+              <ul>
+                <li><a href="https://covid19.who.int/">WHO COVID-19</a></li>
+                <li><a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html">COVID-19 Safety Measures</a></li>
+                <li><a href="https://www.who.int/">WHO</a></li>
+                <li><a href="">Home</a></li>
+              </ul>
+            </div>
+            <div className="bigger-box">
+              <h4>Useful Links</h4>
+              <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+              </ul>
+            </div>
+            <div className="bigger-box">
+              <h4>Useful Links</h4>
+              <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="footer-row-2">
+          <p>&copy; Namtar COVID-19 Tracker 2021</p>
+          <p>Created by: Sarif-Design</p>
+        </div>
+      </footer>
     </div>
   );
 }
